afde1f6a700fed6c32fb3b28ce0326a513b907db
lewis <lewisrjc@gmail.com>
2018-12-02 22:21:36 -0800

    minor amount of cleanup on the netplayer, players and tables both look good

--- celestia/players/NetPlayer.fs
+++ celestia/players/NetPlayer.fs
@@ -1,34 +1,43 @@
-﻿open IMCS
-open Actions
-open System
-open Searching
-open AlphaBeta2
-open AlphaBetaID
-open MTDF
-open DepthFirstSearch
-open Books.TranspositionTable
-open Heuristics
+﻿//(c) Lewis Coates 2018
+//A network player to play minichess
 
-[<EntryPoint>]
-let main args =
+namespace Celestia.Players
 
-    let player = {EvalFun = SimpleCount;
-                  EvalInit = initialSimple;
-                  SearchPrime = MTDFID;
-                  SearchPonder = Ponderer;}
+module NetworkPlayer =
 
-    use conn = new IMCSConnection(@"svcs.cs.pdx.edu",3589)
-    let foo = connect conn
-    let bind = bindName conn "princess" "1234"
-    try 
-        let res = 
-            match args.Length with
-            | 0 -> Some(doOfferGame conn None None None player)
-            | 1 -> Some(doAcceptGame conn (Int32.Parse(args.[0])) None player)
-            | _ -> printfn "failure, invalid args length"
-                   None
-        ()
-    with
-    | ProtocolError(i) -> printfn "fixthat. %s" i
-               
-    0
+    open IMCS
+    open Actions
+    open System
+    open Searching
+    open AlphaBeta2
+    open AlphaBetaID
+    open MTDF
+    open DepthFirstSearch
+    open Books.TranspositionTable
+    open Heuristics
+
+    [<EntryPoint>]
+    let main args =
+
+        let player = {EvalFun = SimpleCount;
+                      EvalInit = initialSimple;
+                      SearchPrime = MTDFID;
+                      SearchPonder = Ponderer;}
+
+        // This needs a real UI at some point
+        use conn = new IMCSConnection(@"svcs.cs.pdx.edu", 3589)
+        // Bind with a name and a default password
+        let bind = bindName conn "princess" "1234"
+        try 
+            let res = 
+                match args.Length with
+                | 0 -> Some(doOfferGame conn None None None player)
+                | 1 ->
+                    Some(doAcceptGame conn (Int32.Parse(args.[0])) None player)
+                | _ -> printfn "failure, invalid args length"
+                       None
+            ()
+        with
+        | ProtocolError(i) -> printfn "fixthat. %s" i
+                   
+        0
